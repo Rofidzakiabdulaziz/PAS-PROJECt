9b2b50602a266f47f50734866e422494d25da4d8
syhruuu <muhammad90635@smk.belajar.id>
2023-12-12 14:48:24 +0700

    perbaruan ni

--- js/guru.js
+++ js/guru.js
@@ -4,7 +4,7 @@ async function getMenuData(){
     console.log(dataApi)
 
    dataApi.Card.forEach((element) => {
-    addMenudata(element.Image , element.Namaguru , element.Mapel , element.Kelas);
+    addMenudata(element.Image , element.Namaguru , element.Mapel );
     console.log(element.Image)
    });
 }
@@ -14,17 +14,18 @@ const row_menu = document.getElementById("row-menu")
 console.log(row_menu) 
 
 
-const addMenudata = (img , judul , mapel , kelas) => {
+const addMenudata = (img , judul , mapel ) => {
     let get = `<div class="col-sm-4 col-12">
     <div class="menu-card">
     <a>
      <div class="menu-icon text-start "><img src="${img}" class="w-100"</div>   
      <h3>Nama:${judul}</h3>
      <h3>Mapel:${mapel}</h3>
-     <div class="row">
-     <a href="show.html" class="col-6 mx-3 p-0">  <button type="button" class="btn btn-outline-dark px-4 me-md-2">Show More</button></a>
-   </div>
+     
      </a>
+     <div class="row">
+     <a href="show.html" class=" mx-3 p-0 ">  <button type="button" class="col-11 btn btn-outline-dark px-4 me-md-2">Show More</button></a>
+   </div>
     </div>
     </div>`;
 
